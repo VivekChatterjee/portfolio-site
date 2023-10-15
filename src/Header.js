61afb3bee81b7ec891500cd275542d070467869b
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2023-10-15 12:51:51 +0530

    Navlink active is added

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, {  useState } from 'react'
+import './App.css'
 
+
+
+const NavLinkCustom = ({text,href,setActive,active}) => {
+  return <a href={href} onClick={()=>setActive(href)} className={active === href ? "active" : ""}>{text}</a>
+}
 
 const Header = () => {
+  const [active,setActive] = useState('/#home')
 
-    
   return (
     <div className="App">
-    <header className="header">
-<a href="\" className="logo"
->Portfolio</a
->
-<i className="bx bx-menu" id="menu-icon"></i>
-<nav className="navbar" >
-<a href="#home" className="active">Home</a>
-<a href="#about">About</a>
-<a href="#services">Services</a>
-<a href="#portfolio">Projects</a>
-<a href="#contact">Contact</a>
-</nav>
-</header>
+        <header className="header navbar">
+        <a href="\" className="logo">Portfolio</a>
+        <nav className="navbar">
+        <NavLinkCustom href="/#home" text="Home" active={active} setActive={setActive}/>
+        <NavLinkCustom href="/#about" text="About" active={active} setActive={setActive}/>
+        <NavLinkCustom href="/#services" text="Services" active={active} setActive={setActive}/>
+        <NavLinkCustom href="/#portfolio" text="Projects" active={active} setActive={setActive}/>
+        <NavLinkCustom href="/#contact" text="Contact" active={active} setActive={setActive} />
+      </nav>
+        </header>
 </div>
   )
 }
